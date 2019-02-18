4b3fd9777714a273d424b93210f00c13e669d250
Lewis Jang <lewis.aedes@gmail.com>
2019-02-18 22:08:53 +0800

    add some demo code for create org entity with modify

--- src/main/java/com/lj/sence/EventHandler.java
+++ src/main/java/com/lj/sence/EventHandler.java
@@ -8,6 +8,8 @@ import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class EventHandler {
 
@@ -19,7 +21,21 @@ public class EventHandler {
 
         player.sendMessage(spawn_message);
 
-        EntityChicken chicken = new EntityChicken(player.world);
+        EntityChicken chicken = new EntityChicken(player.world) {
+            @java.lang.Override
+            public void onUpdate() {
+                super.onUpdate();
+
+                Logger logs = LogManager.getLogger("update");
+
+                // 建立一個一次性使用的 class 且包含著原本本質的物件。
+                // 這樣就無須想要特定修改的物件，就寫一個獨立的實體
+                // 比如只想要叫一隻殭屍，而且這血量特別多 (這情形也可以直接設定參數)
+                // 但如果類似，我想叫出這殭屍，但他不攻擊人類。就需要這樣寫法了。
+                logs.info("this message send by modifies class at create it.");
+            }
+        };
+
         chicken.copyLocationAndAnglesFrom(player);
         player.world.spawnEntity(chicken);
     }
