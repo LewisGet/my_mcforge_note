35b0ad2dc6d3fde9c3da58493657107bf9160963
Lewis Jang <lewis.aedes@gmail.com>
2019-02-18 21:54:12 +0800

    add timer in entity

--- src/main/java/com/lj/sence/Entity/TestChickenEntity.java
+++ src/main/java/com/lj/sence/Entity/TestChickenEntity.java
@@ -6,9 +6,14 @@ import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TestChickenEntity extends EntityMob
 {
+    public long last_execute_time = 0;
+    public long timeout = 1000;
+
     public TestChickenEntity(World worldIn)
     {
         super(worldIn);
@@ -31,5 +36,23 @@ public class TestChickenEntity extends EntityMob
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
         this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityVillager.class, false));
         this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityIronGolem.class, true));
+    }
+
+    @java.lang.Override
+    public void onUpdate()
+    {
+        super.onUpdate();
+
+        long time = new java.util.Date().getTime();
+
+        if (this.last_execute_time + this.timeout < time)
+        {
+            this.last_execute_time = time;
+
+            Logger logs = LogManager.getLogger("update");
+
+            logs.info(time);
+        }
+
     }
 }
